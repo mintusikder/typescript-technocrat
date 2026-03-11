851d455d27b4dc9bfe9509150d2a1fdda6d9bfa4
Mintu Sikder <mintusikder15@gmail.com>
2026-03-11 11:48:40 +0600

    added static

---
+++ module5/src/static.ts
@@ -0,0 +1,20 @@
+class Counter {
+  static count: number = 0;
+
+  increment() {
+    return Counter.count++;
+  }
+  decrement() {
+    return Counter.count--;
+  }
+}
+
+const counter1 = new Counter();
+console.log(counter1.increment());
+console.log(counter1.increment());
+console.log(counter1.increment());
+
+const counter2 = new Counter();
+console.log(counter2.increment());
+console.log(counter2.increment());
+console.log(counter2.increment());
